a692763cf2548b613abdc84c1b818fc344ef64a3
Arun Ganesh <arun.planemad@gmail.com>
2025-12-13 14:49:13 +0530

    Update map-links layout

--- js/map-links.js
+++ js/map-links.js
@@ -65,7 +65,7 @@ export class MapLinks {
     _createModal() {
         const modalHTML = `
             <sl-dialog id="${this.modalId}" label="Map Navigation Links" class="map-links-modal">
-                <div class="map-links-grid">
+                <div class="map-links-container">
                 </div>
                 <sl-button slot="footer" variant="neutral" id="${this.modalId}-close" class="map-links-btn">Close</sl-button>
             </sl-dialog>
@@ -93,7 +93,7 @@ export class MapLinks {
     _showModal() {
         if (!this._modal || !this._map) return;
 
-        const grid = this._modal.querySelector('.map-links-grid');
+        const container = this._modal.querySelector('.map-links-container');
 
         const center = this._map.getCenter();
         const zoom = Math.round(this._map.getZoom());
@@ -102,7 +102,23 @@ export class MapLinks {
 
         const links = this._generateNavigationLinks(lat, lng, zoom);
 
-        grid.innerHTML = links.map(link => this._createLinkCard(link)).join('');
+        const goaLinks = links.filter(link => link.category === 'goa');
+        const globalLinks = links.filter(link => link.category === 'global');
+
+        container.innerHTML = `
+            <div class="map-links-section">
+                <h3 class="map-links-section-title">Goa</h3>
+                <div class="map-links-grid">
+                    ${goaLinks.map(link => this._createLinkCard(link)).join('')}
+                </div>
+            </div>
+            <div class="map-links-section">
+                <h3 class="map-links-section-title">Global</h3>
+                <div class="map-links-grid">
+                    ${globalLinks.map(link => this._createLinkCard(link)).join('')}
+                </div>
+            </div>
+        `;
 
         this._modal.show();
     }
@@ -130,85 +146,100 @@ export class MapLinks {
 
         return [
             {
-                name: 'OpenStreetMap',
-                url: `https://www.openstreetmap.org/?mlat=${lat}&mlon=${lng}#map=${zoom}/${lat}/${lng}&layers=D`,
-                icon: 'https://upload.wikimedia.org/wikipedia/commons/b/b0/Openstreetmap_logo.svg'
-            },
-            {
-                name: 'OpenStreetMap SpyGlass',
-                url: `http://test.osm2pgsql.org/#p=${zoom}/${lat}/${lng}`,
-                icon: 'http://test.osm2pgsql.org/img/spyglass.svg'
-            },
-            {
-                name: 'Google Maps',
-                url: `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`,
-                icon: 'https://upload.wikimedia.org/wikipedia/commons/a/aa/Google_Maps_icon_%282020%29.svg'
-            },
-            {
-                name: 'Google Earth Studio',
-                url: `https://earth.google.com/web/@${lat},${lng},67.01062587a,1688.30584472d,35y,-0h,0t,0r/data=CgwqBggBEgAYAUICCAFCAggASg0I____________ARAA`,
-                text: 'GE'
-            },
-            {
-                name: 'ISRO Bhuvan',
-                url: `https://bhuvanmaps.nrsc.gov.in/?mode=Hybrid#${zoom}/${lat}/${lng}`,
-                icon: './assets/img/icon-bhuvan.png'
-            },
-            {
-                name: 'Bhuvan Data Hub',
-                url: `https://bhuvanmaps.nrsc.gov.in/science?dataHubTab=0&mode=Satellite#${zoom}/${lat}/${lng}`,
-                icon: './assets/img/icon-bhuvan.png'
+                name: 'One Map Goa GIS',
+                url: `https://onemapgoagis.goa.gov.in/map/?ct=LayerTree${oneMapGoaLayerList}&bl=mmi_hybrid&t=goa_default&c=${mercatorCoords.x}%2C${mercatorCoords.y}&s=500`,
+                icon: './assets/img/icon-onemapgoa.png',
+                category: 'goa'
             },
             {
                 name: 'NIC Bharatmaps',
                 url: `https://bharatmaps.gov.in/BharatMaps/Home/Map?long=${lat}&lat=${lng}`,
-                text: 'BM'
+                text: 'BM',
+                category: 'goa'
             },
             {
-                name: 'One Map Goa GIS',
-                url: `https://onemapgoagis.goa.gov.in/map/?ct=LayerTree${oneMapGoaLayerList}&bl=mmi_hybrid&t=goa_default&c=${mercatorCoords.x}%2C${mercatorCoords.y}&s=500`,
-                icon: './assets/img/icon-onemapgoa.png'
+                name: 'ISRO Bhuvan',
+                url: `https://bhuvanmaps.nrsc.gov.in/?mode=Hybrid#${zoom}/${lat}/${lng}`,
+                icon: './assets/img/icon-bhuvan.png',
+                category: 'goa'
             },
             {
-                name: 'ESRI Living Atlas Landcover',
+                name: 'Bhuvan Data Hub',
+                url: `https://bhuvanmaps.nrsc.gov.in/science?dataHubTab=0&mode=Satellite#${zoom}/${lat}/${lng}`,
+                icon: './assets/img/icon-bhuvan.png',
+                category: 'goa'
+            },
+            {
+                name: 'OpenStreetMap',
+                url: `https://www.openstreetmap.org/?mlat=${lat}&mlon=${lng}#map=${zoom}/${lat}/${lng}&layers=D`,
+                icon: 'https://upload.wikimedia.org/wikipedia/commons/b/b0/Openstreetmap_logo.svg',
+                category: 'global'
+            },
+            {
+                name: 'OSM SpyGlass',
+                url: `http://test.osm2pgsql.org/#p=${zoom}/${lat}/${lng}`,
+                icon: 'http://test.osm2pgsql.org/img/spyglass.svg',
+                category: 'global'
+            },
+            {
+                name: 'Google Maps',
+                url: `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`,
+                icon: 'https://upload.wikimedia.org/wikipedia/commons/a/aa/Google_Maps_icon_%282020%29.svg',
+                category: 'global'
+            },
+            {
+                name: 'Google Earth',
+                url: `https://earth.google.com/web/@${lat},${lng},67.01062587a,1688.30584472d,35y,-0h,0t,0r/data=CgwqBggBEgAYAUICCAFCAggASg0I____________ARAA`,
+                text: 'GE',
+                category: 'global'
+            },
+            {
+                name: 'Landcover',
                 url: `https://livingatlas.arcgis.com/landcoverexplorer/#mapCenter=${lng}%2C${lat}%2C${zoom}.79&mode=step&timeExtent=2017%2C2023&year=2023`,
-                text: 'LC'
+                text: 'LC',
+                category: 'global'
             },
             {
-                name: 'Google Earth Engine Timelapse',
+                name: 'Timelapse',
                 url: `https://earthengine.google.com/timelapse#v=${lat},${lng},15,latLng&t=0.41&ps=50&bt=19840101&et=20221231`,
-                text: 'TL'
+                text: 'TL',
+                category: 'global'
             },
             {
-                name: 'FIRMS Fire Information',
+                name: 'Fire Info',
                 url: `https://firms.modaps.eosdis.nasa.gov/map/#d:24hrs;@${lng},${lat},14.00z`,
-                text: 'FR'
+                text: 'FR',
+                category: 'global'
             },
             {
-                name: 'ESA Copernicus Browser',
+                name: 'Copernicus',
                 url: `https://browser.dataspace.copernicus.eu/?zoom=${zoom}&lat=${lat}&lng=${lng}&themeId=DEFAULT-THEME&visualizationUrl=U2FsdGVkX18d3QCo8ly51mKnde%2FbnPTNY3M%2Bvkw2HJS5PZYTtLYG6ZjWVDYuz%2Bszj9bzKcR5Th1mcWjsfJneWz3DM1gd75vRaH%2BioFw2j3mQa79Yj8F7TkWwvb2ow0kh&datasetId=3c662330-108b-4378-8899-525fd5a225cb&fromTime=2024-12-01T00%3A00%3A00.000Z&toTime=2024-12-01T23%3A59%3A59.999Z&layerId=0-RGB-RATIO&demSource3D=%22MAPZEN%22&cloudCoverage=30&dateMode=SINGLE`,
-                text: 'CO'
+                text: 'CO',
+                category: 'global'
             },
             {
-                name: 'ESRI Landsat Explorer',
+                name: 'Landsat',
                 url: `https://livingatlas.arcgis.com/landsatexplorer/#mapCenter=${lng}%2C${lat}%2C${zoom}&mode=dynamic&mainScene=%7CColor+Infrared+for+Visualization%7C`,
-                text: 'LS'
+                text: 'LS',
+                category: 'global'
             },
             {
-                name: 'Zoom Earth Live Weather',
+                name: 'Weather',
                 url: `https://zoom.earth/maps/temperature/#view=${lat},${lng},11z`,
-                text: 'ZE'
+                text: 'ZE',
+                category: 'global'
             },
             {
-                name: 'NASA Worldview Explorer',
+                name: 'Worldview',
                 url: (() => {
                     const bbox = this._calculateBbox(lng, lat, zoom);
                     return `https://worldview.earthdata.nasa.gov/?v=${bbox.west},${bbox.south},${bbox.east},${bbox.north}&l=Reference_Labels_15m(hidden),Reference_Features_15m(hidden),Coastlines_15m(hidden),VIIRS_SNPP_DayNightBand_At_Sensor_Radiance,VIIRS_Black_Marble,VIIRS_SNPP_CorrectedReflectance_TrueColor(hidden),MODIS_Aqua_CorrectedReflectance_TrueColor(hidden),MODIS_Terra_CorrectedReflectance_TrueColor(hidden)&lg=false&t=2021-01-10-T19%3A18%3A03Z`;
                 })(),
-                text: 'WV'
+                text: 'WV',
+                category: 'global'
             },
             {
-                name: 'Global Forest Watch',
+                name: 'Forest Watch',
                 url: `https://www.globalforestwatch.org/map/?map=${encodeURIComponent(JSON.stringify({
                     center: {
                         lat: lat,
@@ -267,7 +298,8 @@ export class MapLinks {
                 }))}&mapMenu=${encodeURIComponent(JSON.stringify({
                     datasetCategory: "landCover"
                 }))}`,
-                text: 'FW'
+                text: 'FW',
+                category: 'global'
             }
         ];
     }
