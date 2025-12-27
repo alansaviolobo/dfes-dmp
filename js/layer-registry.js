b4b2445f32f102607c0a114dd9be568d2d9c6ec9
Alan Lobo <alansaviolobo@gmail.com>
2025-12-27 11:04:02 +0530

    Amche Setting.

--- js/layer-registry.js
+++ js/layer-registry.js
@@ -4,7 +4,7 @@ export class LayerRegistry {
         this._libraryLayers = new Map(); // library layer presets
         this._atlasLayers = new Map(); // atlasId -> array of layer configs
         this._atlasMetadata = new Map(); // atlasId -> atlas metadata (color, name, etc.)
-        this._currentAtlas = 'index'; // default atlas
+        this._currentAtlas = 'dfes-dmp'; // default atlas
         this._initialized = false;
     }
 
